609f1a60511ea7b5c448a863f1b5fb45f70debc9
john-dickinson <johndi@umich.edu>
2015-11-14 17:00:44 -0500

    Dynamic generated content. Add real videos, images, add names, add back button func

--- js/script.js
+++ js/script.js
@@ -3,23 +3,33 @@ $(document).ready(function() {
 	$('.carousel').slick({
 		infinite: true,
 		slidesToShow: 3,
-		slidesToScroll: 2,
+		slidesToScroll: 1,
 		speed: 900,
 		arrows: true
 	});
 
-	var video_dict = {
-		"rella": "https://www.youtube.com/embed/IxLHwL5cax8",
-		"koi": "https://www.youtube.com/embed/IxLHwL5cax8",
-		"wipe": "https://www.youtube.com/embed/IxLHwL5cax8",
-		"bobbi": "https://www.youtube.com/embed/IxLHwL5cax8",
+	var artists = {
+		bobbi: {url:'https://www.youtube.com/embed/IxLHwL5cax8?autoplay=0&showinfo=0&controls=2', artist_name: "Bobbi Palace"},
+		wipe : {url:'https://www.youtube.com/embed/IxLHwL5cax8?autoplay=0&showinfo=0&controls=2', artist_name: "Wipe"},
+		saajtak : {url:'https://www.youtube.com/embed/IxLHwL5cax8?autoplay=0&showinfo=0&controls=2', artist_name: "Saajtak"},
+		gabby : {url:'https://www.youtube.com/embed/IxLHwL5cax8?autoplay=0&showinfo=0&controls=2', artist_name: "Gabby Kirsch"},
+		rella : {url:'https://www.youtube.com/embed/IxLHwL5cax8?autoplay=0&showinfo=0&controls=2', artist_name: "Rella"}
 	}
 
 	var videoAnimationModule = {
-		toVideo: function(){
+		toVideo: function(id){
 			$('.main').animate({left: "5000px"},function(){
 				$('.main').fadeOut('fast',function(){
+					$('.active_video').empty();
+					var video = '<iframe class="mainVideo" src="'+ artists[id].url +'" frameborder="0" allowfullscreen></iframe>';
+					var title = '<h1>' + artists[id].artist_name + '</h1>';
+					$('.active_video').append('<h1 class="cornerPulse">< SESSIONS</h1>');
+					$('.active_video').append(title);
+					$('.active_video').append(video);
 					$('.active_video').fadeIn('fast');
+					$('.cornerPulse').click(function(){
+						videoAnimationModule.toMain();
+					});
 				});
 			});
 		},
@@ -44,11 +54,8 @@ $(document).ready(function() {
 	});
 
 	$('.thumbnail').click(function(event) {
-		videoAnimationModule.toVideo();
-	});
-
-	$('.cornerPulse').click(function(){
-		videoAnimationModule.toMain();
+		var id = $(this).attr('id');
+		videoAnimationModule.toVideo(id);
 	});
 
 });
