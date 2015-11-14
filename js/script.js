3c5f3a8a735635776b5c9145109bffb8b2a7e2e2
john-dickinson <johndi@umich.edu>
2015-11-14 16:12:08 -0500

    band names

--- js/script.js
+++ js/script.js
@@ -1,24 +1,54 @@
 $(document).ready(function() {
 
-		$('.carousel').slick({
+	$('.carousel').slick({
 		infinite: true,
 		slidesToShow: 3,
-		slidesToScroll: 3,
-		dots: true,
+		slidesToScroll: 2,
 		speed: 900,
 		arrows: true
 	});
 
+	var video_dict = {
+		"rella": "https://www.youtube.com/embed/IxLHwL5cax8",
+		"koi": "https://www.youtube.com/embed/IxLHwL5cax8",
+		"wipe": "https://www.youtube.com/embed/IxLHwL5cax8",
+		"bobbi": "https://www.youtube.com/embed/IxLHwL5cax8",
+	}
+
+	var videoAnimationModule = {
+		toVideo: function(){
+			$('.main').animate({left: "5000px"},function(){
+				$('.main').fadeOut('fast',function(){
+					$('.active_video').fadeIn('fast');
+				});
+			});
+		},
+
+		toMain: function(){
+			$('.active_video').fadeOut('fast',function(){
+				$('.main').fadeIn('fast');
+			});
+		}
+	}
+
 	var enterModule = {
 		enter: function(){
 			$('#welcomeWrap').fadeOut('slow',function(){
-				$('.main').fadeIn('slow');
-			})
+				$('.main').css('visibility','visible');
+			});
 		}
 	}
 
 	$('#enterLink').click(function(){
 		enterModule.enter();
+	});
+
+	$('.thumbnail').click(function(event) {
+		videoAnimationModule.toVideo();
+	});
+
+	$('.cornerPulse').click(function(){
+		videoAnimationModule.toMain();
 	});
 
 });
